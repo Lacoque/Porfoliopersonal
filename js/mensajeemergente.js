af5c0f163c7e25e40ff22772e7d51336c0278ee1
Lacoque <pamela.lacoquex@gmail.com>
2024-04-28 17:54:59 -0300

    Update mensajeemergente.js

--- js/mensajeemergente.js
+++ js/mensajeemergente.js
@@ -7,7 +7,6 @@ document.addEventListener("DOMContentLoaded", function () {
     if (formSentInput && formSentInput.value === "1") {
         modal.style.display = "block"; // Mostrar el mensaje emergente
     }
-    
 
     // Cerrar el mensaje emergente al hacer clic en el botón de cierre
     closeButton.addEventListener("click", function () {
@@ -20,5 +19,19 @@ document.addEventListener("DOMContentLoaded", function () {
             modal.style.display = "none"; // Ocultar el mensaje emergente al hacer clic fuera de él
         }
     });
-   
+
+    // Agregar evento de escucha al enviar el formulario
+    document.getElementById("request").addEventListener("submit", function (event) {
+        event.preventDefault(); // Evitar que el formulario se envíe de forma predeterminada
+
+        // Tu lógica de validación del formulario aquí...
+        // Por ejemplo, puedes verificar que todos los campos estén completos antes de enviar el formulario.
+
+        // Si el formulario es válido, cambia el valor del campo formSent a "1"
+        document.getElementById("formSent").value = "1";
+
+        // Envía el formulario
+        this.submit();
+    });
 });
+
