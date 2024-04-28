181b281bfcc2e4fd009290e0391ec2750ae87fbd
Lacoque <pamela.lacoquex@gmail.com>
2024-04-28 17:39:13 -0300

    Update mensajeemergente.js

--- js/mensajeemergente.js
+++ js/mensajeemergente.js
@@ -1,14 +1,12 @@
-// Agrega este código JavaScript para mostrar el mensaje emergente
 document.addEventListener("DOMContentLoaded", function () {
     const modal = document.getElementById("modal");
     const closeButton = document.querySelector(".close");
 
-    // Mostrar el mensaje emergente al enviar el formulario
+    // Mostrar el mensaje emergente si el formulario se envió correctamente
     const formSentInput = document.getElementById("formSent");
     if (formSentInput && formSentInput.value === "1") {
         modal.style.display = "block"; // Mostrar el mensaje emergente
     }
-    
 
     // Cerrar el mensaje emergente al hacer clic en el botón de cierre
     closeButton.addEventListener("click", function () {
